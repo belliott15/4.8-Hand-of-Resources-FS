6a7895ced7dcae510599a6e1ff8cd0672c63b276
Beau Elliott <belliott1516@gmail.com>
2022-06-15 17:45:00 -0700

    got post test to work on horcruxes

--- __tests__/horcrux.test.js
+++ __tests__/horcrux.test.js
@@ -11,6 +11,38 @@ describe('backend-express-template routes', () => {
     const res = await request(app).get('/horcrux');
     expect(res.body.length).toEqual(7);
   });
+
+  it('GET /horcrux/id should return a single horcrux', async () => {
+    const res = await request(app).get('/horcrux/1');
+    expect(res.body.name).toEqual('Tom Riddles Diary');
+  });
+
+  it('POST /horcrux should add a horcrux to the table data', async () => {
+    const res = await await request(app)
+      .post('/horcrux')
+      .send({ name: 'Lord Voldemort', 
+        original_owner: 'Tom Riddle', 
+        house_artifact: false,
+        sacrifice: 'Lord Voldemort'
+      });
+    expect(res.status).toBe(200);
+    expect(res.body.name).toEqual('Lord Voldemort');
+    expect(res.body.id).toEqual('8');
+  });
+
+  it('POST /horcrux should add a horcrux to the table data', async () => {
+    const res = await await request(app)
+      .post('/horcrux')
+      .send({ name: 'Lord Voldemort', 
+        original_owner: 'Tom Riddle', 
+        house_artifact: false,
+        sacrifice: 'Lord Voldemort'
+      });
+    expect(res.status).toBe(200);
+    expect(res.body.name).toEqual('Lord Voldemort');
+    expect(res.body.id).toEqual('8');
+  });
+
   afterAll(() => {
     pool.end();
   });
