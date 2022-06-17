ff4cab4b394fe8d68b074cc522792cad0b309e5a
Beau Elliott <belliott1516@gmail.com>
2022-06-17 14:41:18 -0700

    changed router to connect to express, added houseplants to app.js, and got test to pass

--- __tests__/houseplants.test.js
+++ __tests__/houseplants.test.js
@@ -10,11 +10,13 @@ describe('backend-express-template routes', () => {
   it('GET /houseplant should return a list of all houseplants', async () => {
     const res = await request(app).get('/houseplant');
     const expected = {
+      id: '1',
       name: 'ZZ Plant',
       age: 5,
       species: 'Zamioculcas zamiifolia',
       alive: true
     };
+    expect(res.status).toEqual(200);
     expect(res.body[0]).toEqual(expected);
   });
 
