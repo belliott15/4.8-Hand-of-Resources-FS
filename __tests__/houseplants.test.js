f4d9570fa331a62605a6cf98192ac17c10636995
Beau Elliott <belliott1516@gmail.com>
2022-06-17 14:27:22 -0700

    added test for getting all houseplants data, as well as houseplant controller, model, and testing suite

---
+++ __tests__/houseplants.test.js
@@ -0,0 +1,25 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('backend-express-template routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+  it('GET /houseplant should return a list of all houseplants', async () => {
+    const res = await request(app).get('/houseplant');
+    const expected = {
+      name: 'ZZ Plant',
+      age: 5,
+      species: 'Zamioculcas zamiifolia',
+      alive: true
+    };
+    expect(res.body[0]).toEqual(expected);
+  });
+
+
+  afterAll(() => {
+    pool.end();
+  });
+});
